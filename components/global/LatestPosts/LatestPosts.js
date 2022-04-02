86f327695a348a9a1b76c37c16b6b269e747d898
Calvin T <58273749+Calvin087@users.noreply.github.com>
2022-04-02 22:28:25 +0200

    save

---
+++ components/global/LatestPosts/LatestPosts.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import styles from "./LatestPosts.module.css";
+
+const renderPosts = (post) => {
+  const ex = post.acf.excerpt.split(" ").slice(0, 15).join(" ");
+
+  return (
+    <div className={styles.post_container}>
+      <Link href={`/blog/${post.slug}`}>
+        <a>
+          <div className={styles.post_title}>{post.title.rendered}:</div>
+          <div className={styles.post_ex}> {ex}...</div>
+        </a>
+      </Link>
+    </div>
+  );
+};
+
+const LatestPosts = ({ posts }) => {
+  return (
+    <div>
+      {posts &&
+        posts.map((post, i) => {
+          return <div key={i}>{renderPosts(post)}</div>;
+        })}
+    </div>
+  );
+};
+
+export default LatestPosts;
