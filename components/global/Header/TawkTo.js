7806c22675e9c9c9969d3f013fb1191ab11e0f7e
Calvin <calvin@Calvins-MacBook-Air.local>
2022-07-08 22:01:19 +0200

    save

---
+++ components/global/Header/TawkTo.js
@@ -0,0 +1,9 @@
+export const tawkTo = `var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/60794a0cf7ce1827093b0358/1f3csig2b';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();`;
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
 import "../styles/globals.css";
 import "prismjs/themes/prism-tomorrow.css";
+import { useEffect } from "react";
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+    (function () {
+      var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = "https://embed.tawk.to/60794a0cf7ce1827093b0358/1f3csig2b";
+      s1.charset = "UTF-8";
+      s1.setAttribute("crossorigin", "*");
+      s0.parentNode.insertBefore(s1, s0);
+    })();
+  }, []);
   return <Component {...pageProps} />;
 }
 
